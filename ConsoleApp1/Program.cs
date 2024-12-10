f8f7ace55796fc4968c20e48f26b9b7258443554
exuteboll <exuteboll4@gmail.com>
2024-12-10 11:06:13 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //найти первый и последний элемент в которых длина больше 3 символов 
+            //Задача 7. Поиск элемента по условию.
+            string[] people = { "Tom", "Sam", "BOb", "Kate", "Tom", "Alice" };
+            //string first = Array.Find(people, person => person.Length > 3);
+            //Console.WriteLine(first);
+            //string last = Array.FindLast(people, person => person.Length > 3);
+            //Console.WriteLine(last);
+            //string[] last3 = Array.FindAll(people, person => person.Length == 3);
+            //foreach(var l in last3)
+            //{
+            //    Console.WriteLine(l);
+            //}
+            
+           
+            
+        }
+    }
+}
