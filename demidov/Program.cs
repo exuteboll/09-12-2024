23ba56afc8c641934df4fc1624364b423ee20f5b
exuteboll <exuteboll4@gmail.com>
2024-12-10 10:31:29 +0200

    Add files via upload

---
+++ demidov/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demidov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 7, 1, 9, 5, 3, 6, 3 };
+            //int count = 0;
+            //foreach(int x in array)
+            //{
+            //    if (x == 3)
+            //    { count++; }
+            //}
+            //Console.WriteLine(count);
+            //    Console.ReadKey();
+            //int attay = array.Count(x => x == 3);
+            //Console.WriteLine(attay);
+            Console.ReadKey();
+        }
+    }
+}
