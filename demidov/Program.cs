12e66584237016c4dd59ab3531179dc5ec62bdae
exuteboll <exuteboll4@gmail.com>
2024-12-10 10:17:56 +0200

    Add files via upload

---
+++ demidov/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demidov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+                int[] fruit = { 4, 3, 6, 7 };
+                Array.Sort(fruit);
+            foreach(int i in fruit)
+            {
+                Console.WriteLine(i);
+            }
+                
+                Array.Reverse(fruit);
+            foreach (int i in fruit)
+            {
+                Console.WriteLine(i);
+            }
+            
+                Console.ReadKey();
+            
+        }
+    }
+}
